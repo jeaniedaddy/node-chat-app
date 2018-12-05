b977567f33a3527397b7fdd901a905f5e8fa3761
steve <jeaniedaddy@gmail.com>
2018-12-05 11:47:32 -0500

    format timestamps using mement.js

--- public/js/index.js
+++ public/js/index.js
@@ -14,19 +14,21 @@ socket.on('disconnect',function(){
 }); 
 
 socket.on('newMessage',function(message){
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     console.log('newMessage', message);
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     jQuery('#messages').append(li);
 });
 
-socket.on('newLocationMessage',function(location){
-    console.log('newLocationMessage', location);
+socket.on('newLocationMessage',function(message){
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    console.log('newLocationMessage', message);
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My Location</a>');
-    a.attr('href', location.url)
+    a.attr('href', message.url)
 
-    li.text(`${location.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     li.append(a);
     
     jQuery('#messages').append(li);
